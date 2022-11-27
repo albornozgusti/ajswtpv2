cf073334b4aba6d00847a2788abe994854c34bde
Gustavo Albornoz <albornozgusti@gmail.com>
2022-11-27 00:43:00 -0300

    initial commit

---
+++ src/main/java/com/example/ajswtpv2/Ajswtpv2Application.java
@@ -0,0 +1,13 @@
+package com.example.ajswtpv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ajswtpv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ajswtpv2Application.class, args);
+    }
+
+}
